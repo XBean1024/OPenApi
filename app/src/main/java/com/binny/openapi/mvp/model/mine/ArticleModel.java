a07aa1fe6eae82b723a5342c6cfa4ce0a4221482
Xbean1024 <596928539@qq.com>
2018-05-25 14:24:46 +0800

    retrofit is ok

--- app/src/main/java/com/binny/openapi/mvp/model/mine/ArticleModel.java
+++ app/src/main/java/com/binny/openapi/mvp/model/mine/ArticleModel.java
@@ -6,8 +6,16 @@ import com.bean.xhttp.response.Response;
 import com.binny.openapi.mvp.bean.ArticleBean;
 import com.binny.openapi.mvp.callback.OnArticleCallback;
 import com.binny.openapi.mvp.ui.fragment.home.vpfragment.IArticleView;
+import com.binny.openapi.retrofit.api.IArticleService;
 import com.binny.openapi.util.UtilsLog;
 import com.google.gson.Gson;
+
+import io.reactivex.Observer;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
+
+import static com.binny.openapi.util.RetrofitServiceUtil.getArticleService;
 
 /**
  * author  binny
@@ -21,46 +29,46 @@ public class ArticleModel {
     }
 
     public void getArticle() {
-//        IArticleService service = getArticleService();
-//        service.getArticleToday()
-//                .subscribeOn(Schedulers.io())
-//                .observeOn(AndroidSchedulers.mainThread())
-//                .subscribe(new Observer<ArticleBean>() {
-//                    @Override
-//                    public void onSubscribe(final Disposable d) {
-//                        articleCallback.onLoading();
-//                    }
-//
-//                    @Override
-//                    public void onNext(final ArticleBean articleBean) {
-//                        articleCallback.onGranted(articleBean);
-//                    }
-//
-//                    @Override
-//                    public void onError(final Throwable e) {
-//                        articleCallback.onError(e.getMessage());
-//                    }
-//
-//                    @Override
-//                    public void onComplete() {
-//                            articleCallback.onLoadDone();
-//                    }
-//                });
-        XHttp.getInstance()
-                .get("https://interface.meiriyiwen.com/article/today?dev=1")
-                .setOnXHttpCallback(new OnXHttpCallback() {
+        IArticleService service = getArticleService();
+        service.getArticleToday()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Observer<ArticleBean>() {
                     @Override
-                    public void onSuccess(Response response) {
-                        Gson gson = new Gson();
-                        ArticleBean articleBean = gson.fromJson(response.toString(), ArticleBean.class);
+                    public void onSubscribe(final Disposable d) {
+                        mCallback.onLoading();
+                    }
+
+                    @Override
+                    public void onNext(final ArticleBean articleBean) {
                         mCallback.onSuccess(articleBean);
                     }
 
                     @Override
-                    public void onFailure(Exception ex, String errorCode) {
-                        UtilsLog.i("ArticleModel","onDeny"+ex.getMessage());
+                    public void onError(final Throwable e) {
+                        mCallback.onError(e.getMessage());
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        mCallback.onLoadDone();
                     }
                 });
+//        XHttp.getInstance()
+//                .get("https://interface.meiriyiwen.com/article/today?dev=1")
+//                .setOnXHttpCallback(new OnXHttpCallback() {
+//                    @Override
+//                    public void onSuccess(Response response) {
+//                        Gson gson = new Gson();
+//                        ArticleBean articleBean = gson.fromJson(response.toString(), ArticleBean.class);
+//                        mCallback.onSuccess(articleBean);
+//                    }
+//
+//                    @Override
+//                    public void onFailure(Exception ex, String errorCode) {
+//                        UtilsLog.i("ArticleModel","onDeny"+ex.getMessage());
+//                    }
+//                });
     }
 
     public void getArticleRandom() {
--- app/src/main/java/com/binny/openapi/retrofit/api/IArticleService.java
+++ app/src/main/java/com/binny/openapi/retrofit/api/IArticleService.java
@@ -4,6 +4,8 @@ import com.binny.openapi.mvp.bean.ArticleBean;
 
 import io.reactivex.Observable;
 import retrofit2.http.GET;
+import retrofit2.http.HEAD;
+import retrofit2.http.Headers;
 import retrofit2.http.Query;
 
 /**
@@ -22,17 +24,20 @@ public interface IArticleService {
     * 获取 每日一文 接口 :当天的
     * */
     @GET("article/today")
+    @Headers("User-Agent: Retrofit-App")
     Observable<ArticleBean> getArticleToday();
 
     /*
     * 获取 每日一文 接口 :随机的
     * */
     @GET("article/random")
+    @Headers("User-Agent: Retrofit-App")
     Observable<ArticleBean> getArticleRandom();
 
     /*
     * 获取 每日一文 接口 :指定某一天
     * */
     @GET("article/day?dev=1")
+    @Headers("User-Agent: Retrofit-App")
     Observable<ArticleBean> getArticleDay(@Query("date") int date);
 }
