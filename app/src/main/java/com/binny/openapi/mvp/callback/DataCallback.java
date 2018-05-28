c32e330c17fb33a4ce488995082d82818d76d1f5
Xbean1024 <596928539@qq.com>
2018-05-28 09:16:27 +0800

    删除赘余接口

--- app/src/main/java/com/binny/openapi/mvp/callback/DataCallback.java
+++ app/src/main/java/com/binny/openapi/mvp/callback/DataCallback.java
@@ -10,6 +10,8 @@ import java.io.Serializable;
  * author  binny
  * date 5/9
  * 请求成功和失败的接口
+ *
+ *  T 为 请求的实体类型
  */
 public interface DataCallback<T extends Serializable> extends OnSuccessCallback<T>, OnErrorCallback,ILoadingView {
 }
